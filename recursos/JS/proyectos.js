25dd8bb3e8827ed30386775d6dff046285d30059
lugengar <luciano.barbinii@gmail.com>
2025-02-05 20:42:41 -0300

    a

--- recursos/JS/proyectos.js
+++ recursos/JS/proyectos.js
@@ -8,21 +8,25 @@ fetch('recursos/JSON/proyectos.json')
         
         data.forEach(servicio => {
             const divServicio = document.createElement('div');
+            const iframe = document.createElement('iframe');
             divServicio.classList.add('proyecto');
-            divServicio.style.backgroundImage = `url(${ubicacioncarpeta+servicio.imagen})`;
-            divServicio.style.backgroundPosition = servicio.posicionimagen;
+            //divServicio.style.backgroundImage = `url(${ubicacioncarpeta+servicio.imagen})`;
+            //divServicio.style.backgroundPosition = servicio.posicionimagen;
             
             const divBlur = document.createElement('div');
             divBlur.classList.add('blur');
+            iframe.classList.add('proyectoiframe');
+            iframe.src = servicio.url;
             
             const h1Titulo = document.createElement('h1');
             h1Titulo.classList.add('subtitulo');
             h1Titulo.textContent = servicio.titulo;
-            divServicio.innerHTML= `<a class="botonegro" href="${servicio.url}">VISITAR LA WEB</a>`
+            divServicio.innerHTML= `<a class="botonegro" target="_blank" href="${servicio.url}">VISITAR LA WEB</a>`
             const pTexto = document.createElement('p');
             pTexto.textContent = servicio.texto;
             
             divServicio.appendChild(divBlur);
+            divServicio.appendChild(iframe);
             divServicio.appendChild(h1Titulo);
             divServicio.appendChild(pTexto);
             
