fa80eb36228d099af7f972f76d6343bf4d27e6eb
lugengar <luciano.barbinii@gmail.com>
2025-04-19 21:29:27 -0300

    a

---
+++ recursos/JS/proyectos2.js
@@ -0,0 +1,36 @@
+fetch('recursos/JSON/proyectos.json') 
+
+    .then(response => response.json())
+    .then(info => {
+        ubicacioncarpeta = info.configuracion.ubicacioncarpeta
+        data = info.imagenes
+        const listaServicios = document.querySelector('.listaproyectos');
+        
+        data.forEach(servicio => {
+            const divServicio = document.createElement('details');
+            const titulo = document.createElement('summary');
+            const imegen = document.createElement('img');
+     
+            divServicio.classList.add('proyecto2');
+          
+            
+       
+
+            titulo.textContent = servicio.titulo;
+            imegen.src = ubicacioncarpeta+servicio.imagen;
+           const pTexto = document.createElement('p');
+           pTexto.textContent = servicio.texto;
+           divServicio.appendChild(titulo);
+
+           divServicio.appendChild(imegen);
+
+           divServicio.appendChild(pTexto);
+        
+           
+           divServicio.innerHTML +=`<a class="botonegro" target="_blank" href="${servicio.url}">VISITAR LA WEB</a>`
+            
+            listaServicios.appendChild(divServicio);
+        });
+    })
+    .catch(error => console.error('Error cargando el JSON:', error));
+
