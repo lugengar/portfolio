cfbd1f11736f1622e2fa8c69091965d292bc95d0
lugengar <luciano.barbinii@gmail.com>
2025-04-19 22:05:58 -0300

    a

--- recursos/JS/scroll.js
+++ recursos/JS/scroll.js
@@ -9,7 +9,7 @@ function isElementInViewport(el, offset = 250) {
 }
 
 function handleScroll() {
-    const elements = document.querySelectorAll('.titulo, .texto, .proyecto2');
+    const elements = document.querySelectorAll('.titulo, .texto');
     elements.forEach((element) => {
         if (isElementInViewport(element, 150)) { 
             element.classList.add('aparece');
