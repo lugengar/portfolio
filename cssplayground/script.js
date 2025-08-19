f135382357a3bfa9b8867dc8235b10d17b5a095b
lugengar <luciano.barbinii@gmail.com>
2025-08-19 20:44:30 -0300

    a

--- cssplayground/script.js
+++ cssplayground/script.js
@@ -7,6 +7,7 @@ let nivel = [];
 const params = new URLSearchParams(window.location.search);
 
 const numnivel = parseInt(params.get("nivel"), 10) || 1;
+if (numnivel >= 16){numnivel = 1}
 editor.addEventListener("keydown", function(e) {
     if (e.key === "Tab") {
         e.preventDefault();
